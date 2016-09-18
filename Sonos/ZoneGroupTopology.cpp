c94ceb403d7eb89595bc3d042670aa7e4896698f
Christian Schuster <blackyle@me.com>
2016-09-18 23:42:32 +0200

    perform strict checks on zone topology response

--- Sonos/ZoneGroupTopology.cpp
+++ Sonos/ZoneGroupTopology.cpp
@@ -40,7 +40,7 @@ void replaceXmlEntities(String &s) {
 	// TODO replace unicode entities with UTF-8 bytes
 }
 
-bool extractEncodedTags(Stream &stream, std::function<void(String tag)> callback) {
+bool extractEncodedTags(Stream &stream, std::function<bool(String tag)> callback) {
 	// skip everything up to (and including) the next &lt;
 	while (stream.find("&lt;")) {
 		String tag = "&lt;";
@@ -53,7 +53,10 @@ bool extractEncodedTags(Stream &stream, std::function<void(String tag)> callback
 			if (!endMarkerCh) {
 				// tag is complete, replace the XML entities
 				replaceXmlEntities(tag);
-				callback(tag);
+				if (!callback(tag)) {
+					Serial.println(F("Callback returned false"));
+					return false;
+				}
 				break;
 			}
 
@@ -97,35 +100,68 @@ bool ZoneGroupTopology::GetZoneGroupState_Decoded(ZoneInfoCallback callback, boo
 
 	WiFiClient stream = client.getStream();
 
-	bool result = extractEncodedTags(stream, [callback, visibleOnly](String tag) {
+	bool result = extractEncodedTags(stream, [callback, visibleOnly](String tag) -> bool {
 		if (tag.startsWith(F("<Satellite ")) || tag.startsWith(F("<ZoneGroupMember "))) {
+			Serial.print(F("Found tag: "));
+			Serial.println(tag);
 			ZoneInfo info;
 
 			bool visible = tag.indexOf(F(" Invisible=\"1\"")) < 0;
 
-			// TODO fail if one of the "indexOf" calls returns -1
-
 			if (visible || !visibleOnly) {
 				int uuidStart = tag.indexOf(F(" UUID=\"")) + 7;
+				if (uuidStart < 0) {
+					Serial.println(F("Failed to find start of UUID in tag"));
+					return false;
+				}
 				int uuidEnd = tag.indexOf('"', uuidStart);
+				if (uuidEnd < 0) {
+					Serial.println(F("Failed to find end of UUID in tag"));
+					return false;
+				}
 				info.uuid = tag.substring(uuidStart, uuidEnd);
 				replaceXmlEntities(info.uuid);
 
 				int zoneNameStart = tag.indexOf(F(" ZoneName=\"")) + 11;
+				if (zoneNameStart < 0) {
+					Serial.println(F("Failed to find start of zone name in tag"));
+					return false;
+				}
 				int zoneNameEnd = tag.indexOf('"', zoneNameStart);
+				if (zoneNameEnd < 0) {
+					Serial.println(F("Failed to find end of zone name in tag"));
+					return false;
+				}
 				info.name = tag.substring(zoneNameStart, zoneNameEnd);
 				replaceXmlEntities(info.name);
 
 				int locationStart = tag.indexOf(F(" Location=\"")) + 11;
+				if (locationStart < 0) {
+					Serial.println(F("Failed to find start of location in tag"));
+					return false;
+				}
 				int playerIPStart = tag.indexOf(F("//"), locationStart) + 2;
+				if (playerIPStart < 0) {
+					Serial.println(F("Failed to find end of player IP in tag"));
+					return false;
+				}
 				int playerIPEnd = tag.indexOf(':', playerIPStart);
-				info.playerIP.fromString(tag.substring(playerIPStart, playerIPEnd));
+				if (playerIPEnd < 0) {
+					Serial.println(F("Failed to find end of player IP in tag"));
+					return false;
+				}
+				if (!info.playerIP.fromString(tag.substring(playerIPStart, playerIPEnd))) {
+					Serial.println(F("Failed to parse player IP"));
+					return false;
+				}
 
 				info.visible = visible;
 
 				callback(info);
 			}
 		}
+
+		return true;
 	});
 
 	client.end();
