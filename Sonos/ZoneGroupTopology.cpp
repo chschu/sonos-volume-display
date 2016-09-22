bfe9c780faf53fc6174e36612dae2ecbd0ff4c4d
Christian Schuster <blackyle@me.com>
2016-09-22 22:37:38 +0200

    flatten nested conditions

--- Sonos/ZoneGroupTopology.cpp
+++ Sonos/ZoneGroupTopology.cpp
@@ -49,65 +49,65 @@ bool ZoneGroupTopology::GetZoneGroupState_Decoded(ZoneInfoCallback callback, boo
 
 			bool result = XML::extractEncodedTags(client.getStream(), "</ZoneGroupState>",
 					[callback, visibleOnly](String tag) -> bool {
-						if (tag.startsWith(F("<Satellite ")) || tag.startsWith(F("<ZoneGroupMember "))) {
-							Serial.print(F("Found tag: "));
-							Serial.println(tag);
-							ZoneInfo info;
-
-							bool visible = tag.indexOf(F(" Invisible=\"1\"")) < 0;
-
-							if (visible || !visibleOnly) {
-								int uuidStart = tag.indexOf(F(" UUID=\"")) + 7;
-								if (uuidStart < 0) {
-									Serial.println(F("Failed to find start of UUID in tag"));
-									return false;
-								}
-								int uuidEnd = tag.indexOf('"', uuidStart);
-								if (uuidEnd < 0) {
-									Serial.println(F("Failed to find end of UUID in tag"));
-									return false;
-								}
-								info.uuid = tag.substring(uuidStart, uuidEnd);
-								XML::replaceEntities(info.uuid);
-
-								int zoneNameStart = tag.indexOf(F(" ZoneName=\"")) + 11;
-								if (zoneNameStart < 0) {
-									Serial.println(F("Failed to find start of zone name in tag"));
-									return false;
-								}
-								int zoneNameEnd = tag.indexOf('"', zoneNameStart);
-								if (zoneNameEnd < 0) {
-									Serial.println(F("Failed to find end of zone name in tag"));
-									return false;
-								}
-								info.name = tag.substring(zoneNameStart, zoneNameEnd);
-								XML::replaceEntities(info.name);
-
-								int locationStart = tag.indexOf(F(" Location=\"")) + 11;
-								if (locationStart < 0) {
-									Serial.println(F("Failed to find start of location in tag"));
-									return false;
-								}
-								int playerIPStart = tag.indexOf(F("//"), locationStart) + 2;
-								if (playerIPStart < 0) {
-									Serial.println(F("Failed to find end of player IP in tag"));
-									return false;
-								}
-								int playerIPEnd = tag.indexOf(':', playerIPStart);
-								if (playerIPEnd < 0) {
-									Serial.println(F("Failed to find end of player IP in tag"));
-									return false;
-								}
-								if (!info.playerIP.fromString(tag.substring(playerIPStart, playerIPEnd))) {
-									Serial.println(F("Failed to parse player IP"));
-									return false;
-								}
-
-								info.visible = visible;
-
-								callback(info);
-							}
+						if (!tag.startsWith(F("<Satellite ")) && !tag.startsWith(F("<ZoneGroupMember "))) {
+							/* continue tag extraction */
+							return true;
 						}
+
+						ZoneInfo info;
+						info.visible = tag.indexOf(F(" Invisible=\"1\"")) < 0;
+						if (!info.visible && visibleOnly) {
+							/* continue tag extraction */
+							return true;
+						}
+
+						int uuidStart = tag.indexOf(F(" UUID=\"")) + 7;
+						if (uuidStart < 0) {
+							Serial.println(F("Failed to find start of UUID in tag"));
+							return false;
+						}
+						int uuidEnd = tag.indexOf('"', uuidStart);
+						if (uuidEnd < 0) {
+							Serial.println(F("Failed to find end of UUID in tag"));
+							return false;
+						}
+						info.uuid = tag.substring(uuidStart, uuidEnd);
+						XML::replaceEntities(info.uuid);
+
+						int zoneNameStart = tag.indexOf(F(" ZoneName=\"")) + 11;
+						if (zoneNameStart < 0) {
+							Serial.println(F("Failed to find start of zone name in tag"));
+							return false;
+						}
+						int zoneNameEnd = tag.indexOf('"', zoneNameStart);
+						if (zoneNameEnd < 0) {
+							Serial.println(F("Failed to find end of zone name in tag"));
+							return false;
+						}
+						info.name = tag.substring(zoneNameStart, zoneNameEnd);
+						XML::replaceEntities(info.name);
+
+						int locationStart = tag.indexOf(F(" Location=\"")) + 11;
+						if (locationStart < 0) {
+							Serial.println(F("Failed to find start of location in tag"));
+							return false;
+						}
+						int playerIPStart = tag.indexOf(F("//"), locationStart) + 2;
+						if (playerIPStart < 0) {
+							Serial.println(F("Failed to find end of player IP in tag"));
+							return false;
+						}
+						int playerIPEnd = tag.indexOf(':', playerIPStart);
+						if (playerIPEnd < 0) {
+							Serial.println(F("Failed to find end of player IP in tag"));
+							return false;
+						}
+						if (!info.playerIP.fromString(tag.substring(playerIPStart, playerIPEnd))) {
+							Serial.println(F("Failed to parse player IP"));
+							return false;
+						}
+
+						callback(info);
 
 						return true;
 					});
