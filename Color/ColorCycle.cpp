67faa08f4e8e6407303e04f50f8ab0598fe711f2
Christian Schuster <blackyle@me.com>
2017-04-12 23:02:23 +0200

    use std::unique_ptr to manage dynamically allocated array

--- Color/ColorCycle.cpp
+++ Color/ColorCycle.cpp
@@ -10,15 +10,10 @@ namespace Color {
 ColorCycle::ColorCycle(uint16_t length, uint16_t rOffset, uint16_t gOffset, uint16_t bOffset) :
 		_length(length), _colors(new RGB[length]) {
 	for (int i = 0; i < length; i++) {
-		uint8_t r = 127.5 * (1.0 + sin(2.0 * PI * (i + rOffset) / length));
-		uint8_t g = 127.5 * (1.0 + sin(2.0 * PI * (i + gOffset) / length));
-		uint8_t b = 127.5 * (1.0 + sin(2.0 * PI * (i + bOffset) / length));
-		_colors[i] = {r, g, b};
+		_colors[i].red = 127.5 * (1.0 + sin(2.0 * PI * (i + rOffset) / length));
+		_colors[i].green = 127.5 * (1.0 + sin(2.0 * PI * (i + gOffset) / length));
+		_colors[i].blue = 127.5 * (1.0 + sin(2.0 * PI * (i + bOffset) / length));
 	}
-}
-
-ColorCycle::~ColorCycle() {
-	delete[] _colors;
 }
 
 RGB ColorCycle::get(uint16_t pos) const {
--- Color/ColorCycle.h
+++ Color/ColorCycle.h
@@ -2,6 +2,7 @@
 #define COLOR_COLORCYCLE_H_
 
 #include <cstdint>
+#include <memory>
 
 #include "Pattern.h"
 
@@ -10,13 +11,12 @@ namespace Color {
 class ColorCycle: public Pattern {
 public:
 	ColorCycle(uint16_t length, uint16_t rOffset, uint16_t gOffset, uint16_t bOffset);
-	~ColorCycle();
 
 	RGB get(uint16_t pos) const;
 
 private:
 	uint16_t _length;
-	RGB *_colors;
+	std::unique_ptr<RGB[]> _colors;
 };
 
 } /* namespace Color */
